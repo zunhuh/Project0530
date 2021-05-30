b3519208e4ee55204e813acc9aa2acaef7a61e1a
zunhuh <85054258+zunhuh@users.noreply.github.com>
2021-05-30 17:56:06 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	void Start () {
+        print("hello world");
+	}
+
+	void Update () {
+		
+	}
+}
